5af0444fb33e8e4f5869d44750e849e1e0a3a573
hsinha1110 <himanshusinha1110@gmail.com>
2026-02-20 12:06:06 +0530

    useStateHook

--- src/components/class&functional/State.tsx
+++ src/components/class&functional/State.tsx
@@ -1,17 +1,44 @@
-import { View, Button } from 'react-native';
+import { View, Button, Text, TouchableOpacity } from 'react-native';
 import React, { FC, useState } from 'react';
-/* The React useState Hook allows us to track state in a function component. */
+/*  The React useState Hook allows us to track state in a function component */
 const State: FC = () => {
   const [bgColor, setBgColor] = useState('red');
-  console.log(bgColor);
-  const handleBackground = () => {
+  const [switchOff, setSwitchOff] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    name: 'Himanshu',
+    email: 'himanshusinha011@gmail.com',
+    age: 35,
+  });
+
+  const handleDetails = () => {
+    setUserInfo({
+      name: 'Dakshit',
+      email: 'dakshit@gmail.com',
+      age: 6,
+    });
     setBgColor('green');
   };
 
   return (
     <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <View style={{ width: 100, height: 100, backgroundColor: bgColor }} />
-      <Button title="Change Background" onPress={() => handleBackground()} />
+      <View style={{ padding: 10, backgroundColor: bgColor }}>
+        <Text style={{ color: bgColor ? 'white' : 'black' }}>
+          Name: {userInfo.name}
+        </Text>
+        <Text style={{ color: bgColor ? 'white' : 'black' }}>
+          Email: {userInfo.email}
+        </Text>
+        <Text style={{ color: bgColor ? 'white' : 'black' }}>
+          Age: {userInfo.age}
+        </Text>
+        <TouchableOpacity onPress={() => setSwitchOff(!switchOff)}>
+          <Text style={{ color: bgColor ? 'white' : 'black' }}>
+            {switchOff ? 'Off' : 'On'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Button title="Change Details" onPress={handleDetails} />
     </View>
   );
 };
